9d160e544b8f0876b4d56d0a976d211d33d94138
Luka Alhonen <luka.alhonen@protonmail.com>
2021-12-18 23:51:49 +0200

    Added solution for 2.12

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = ({ name, region}) => (
+const Country = ({ name }) => (
   <div>
-    {name} {region}
+    {name}
   </div>
 )
+/*
+
+*/
+
+const CountryDetails = ({ country }) => {
+  const languages = Object.values(country.languages)
+  return (
+  <div>
+    <h1>{country.name.common}</h1>
+    <div>capital {country.capital}</div>
+    <div>population {country.population}</div>
+    <h2>languages</h2>
+    <ul>
+      {languages.map((language, i) =>
+          <li key={i}>
+            {language}
+          </li>
+      )}
+    </ul>
+    <img src={country.flags.png} alt="no flag"/>
+  </div>
+  )
+}
 
 const Filter = (props) => {
   return (
@@ -28,17 +51,26 @@ const App = () => {
   }, [])
 
   const handleFilter = (event) => setFilter(event.target.value)
+  const getCountries = (cs) => {
+    let retval = cs.map(country =>
+      <Country key={countries.indexOf(country)} name={country.name.common} />
+    )
+    if(cs.length > 10){
+      retval = <div>Too many matches, specify another filter</div>
+    } else if(cs.length === 1){
+      retval = <CountryDetails country={cs[0]} />
+    }
+    return retval
+  }
 
-  const countriesToShow = countries.filter(
-    country => country.name.common.toLowerCase().includes(filter.toLowerCase())
+  const countriesToShow = getCountries(countries.filter(
+    country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
   )
 
   return(
     <div>
       <Filter filter={filter} handleFilter={handleFilter} />
-      {countriesToShow.map(country =>
-        <Country key={countries.indexOf(country)} name={country.name.common} />
-      )}
+      {countriesToShow}
     </div>
   )
 }
